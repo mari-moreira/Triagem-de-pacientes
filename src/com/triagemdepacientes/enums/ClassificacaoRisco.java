3f6ec64687f5e11699896fb7455c533661bcff42
Mariana Moreira <moreiramarimana137@gmail.com>
2025-07-16 18:19:22 -0300

    feat: melhoria no enum

--- src/com/triagemdepacientes/enums/ClassificacaoRisco.java
+++ src/com/triagemdepacientes/enums/ClassificacaoRisco.java
@@ -1,18 +1,27 @@
 package com.triagemdepacientes.enums;
 
-//TODO: Testar a cor 
 public enum ClassificacaoRisco {
-	EMERGENCIA("E", 4),
-	MUITO_URGENTE("M", 10), 
-	URGENTE("U", 50), POUCO_URGENTE("P", 120),
-	NAO_URGENTE("N", 240);
+	// Agora passamos 3 valores: a descrição, a sigla e o tempo
+	EMERGENCIA("Emergência", "E", 4),
+	MUITO_URGENTE("Muito Urgente", "M", 10), 
+	URGENTE("Urgente", "U", 50), 
+	POUCO_URGENTE("Pouco Urgente", "P", 120),
+	NAO_URGENTE("Não Urgente", "N", 240);
 
+	private final String descricao; 
 	private final String sigla;
 	private final int tempo;
 
-	private ClassificacaoRisco(String sigla, int tempo) {
+
+	private ClassificacaoRisco(String descricao, String sigla, int tempo) {
+		this.descricao = descricao;
 		this.sigla = sigla;
 		this.tempo = tempo;
+	}
+
+	// Novo getter para a descrição
+	public String getDescricao() {
+		return descricao;
 	}
 
 	public String getSigla() {
@@ -22,5 +31,4 @@ public enum ClassificacaoRisco {
 	public int getTempo() {
 		return tempo;
 	}
-
 }
