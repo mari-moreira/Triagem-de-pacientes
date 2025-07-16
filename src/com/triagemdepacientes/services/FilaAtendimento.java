87a25cc94510bfa075e11482ccb199ae98217b4a
Mariana Moreira <moreiramarimana137@gmail.com>
2025-07-16 18:21:03 -0300

    feat: adiciona o serviço de gerenciamento das filas

---
+++ src/com/triagemdepacientes/services/FilaAtendimento.java
@@ -0,0 +1,69 @@
+package com.triagemdepacientes.services;
+
+import java.time.LocalDateTime;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+import com.triagemdepacientes.enums.ClassificacaoRisco;
+import com.triagemdepacientes.model.*;
+
+public class FilaAtendimento {
+	private final Map<ClassificacaoRisco, Queue<Paciente>> filas;
+	private final Map<ClassificacaoRisco, Integer> contadoresSenha;
+	private final Set<String> cpfsRegistrados;
+
+	public FilaAtendimento() {
+
+		this.filas = new EnumMap<>(ClassificacaoRisco.class);
+		this.contadoresSenha = new EnumMap<>(ClassificacaoRisco.class);
+		this.cpfsRegistrados = new HashSet<>();
+
+		// Fila vazia
+		for (ClassificacaoRisco c : ClassificacaoRisco.values()) {
+			filas.put(c, new LinkedList<>());
+			contadoresSenha.put(c, 1);
+		}
+		
+	}
+	// Add Paciente
+	public  String adicionarPaciente(Paciente paciente, ClassificacaoRisco classificacaoRisco) {
+		if(cpfsRegistrados.contains(paciente.getCpf())) 
+			throw new IllegalArgumentException("CPF já foi Cadastrado");
+		
+		// SEnha de atendimento
+		int sequenciaSenha = contadoresSenha.get(classificacaoRisco);
+		String senha = classificacaoRisco.getSigla() + "." + sequenciaSenha;
+		paciente.setSenha(senha);
+		paciente.setHorarioEnfileiramento(LocalDateTime.now());
+		
+		Queue<Paciente> filaAdequada = this.filas.get(classificacaoRisco);
+		filaAdequada.add(paciente);
+		
+		contadoresSenha.put(classificacaoRisco, sequenciaSenha + 1);
+		cpfsRegistrados.add(paciente.getCpf());
+		
+		return senha;
+	}
+	
+	//TODO: Colocar isso no menu
+	public void visualizarFilas() {
+	       System.out.println("\n==============================================");
+	        System.out.println("        ESTADO ATUAL DE TODAS AS FILAS");
+	        System.out.println("==============================================");
+	        
+	        for(ClassificacaoRisco c : ClassificacaoRisco.values()) {
+	        	System.out.println("\n --- FILA: " + c.getDescricao()+ "---");
+	        	Queue<Paciente> fila = filas.get(c);
+	        	if(fila.isEmpty())
+	        		System.out.println(" [ FILA VAZIA] ");
+	        	else {
+	        		fila.forEach(System.out::println);
+	        	}
+	        }
+	        System.out.println("==============================================");   
+	}
+}
