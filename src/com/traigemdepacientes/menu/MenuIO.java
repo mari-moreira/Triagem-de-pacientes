fcd766299b2dd80c66562a841228227b3b2ccb78
Mariana Moreira <moreiramarimana137@gmail.com>
2025-07-16 18:16:40 -0300

    feat: adicionar opção para visualizar as filas de atendimento

--- src/com/traigemdepacientes/menu/MenuIO.java
+++ src/com/traigemdepacientes/menu/MenuIO.java
@@ -6,7 +6,8 @@ public class MenuIO {
 	 public static int menu(Scanner sc) {
 		 
 		 System.out.print("*** MENU PRINCIPAL***\n \n");
-		 System.out.println("Escolha uma " + "opção de 1 -5 \n" + "1 - CADASTRAR PACIENTE \n"  + "5 - SAIR\n");
+		 System.out.println("Escolha uma " + "opção de 1 -5 \n" + "1 - CADASTRAR PACIENTE \n"  + "2- VISUALIZAR FILAS \n" 
+		 +"5 - SAIR\n");
 						return sc.nextInt();
 		
 	 }
