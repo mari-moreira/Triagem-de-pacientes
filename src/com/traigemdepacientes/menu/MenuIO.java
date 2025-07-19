e83d2c2e0407787a549574408839fd78f609d8f4
Mariana Moreira <moreiramarimana137@gmail.com>
2025-07-18 23:24:14 -0300

    refactor: altera fonte do titulo , adiciona mais opções ao menu e adicione cores nas fontes

--- src/com/traigemdepacientes/menu/MenuIO.java
+++ src/com/traigemdepacientes/menu/MenuIO.java
@@ -2,12 +2,18 @@ package com.traigemdepacientes.menu;
 
 import java.util.Scanner;
 
+import com.triagemdepacientes.util.TerminalColor;
+
 public class MenuIO {
 	 public static int menu(Scanner sc) {
 		 
-		 System.out.print("*** MENU PRINCIPAL***\n \n");
-		 System.out.println("Escolha uma " + "opção de 1 -5 \n" + "1 - CADASTRAR PACIENTE \n"  + "2- VISUALIZAR FILAS \n" 
-		 +"5 - SAIR\n");
+		 System.out.print(TerminalColor.CYAN_BOLD+ "\r\n"
+		 		+ "█▀▄▀█ █▀▀ █▄░█ █░█   █▀█ █▀█ █ █▄░█ █▀▀ █ █▀█ ▄▀█ █░░\r\n"
+		 		+ "█░▀░█ ██▄ █░▀█ █▄█   █▀▀ █▀▄ █ █░▀█ █▄▄ █ █▀▀ █▀█ █▄▄ " +  TerminalColor.RESET + "\n \n");
+		 System.out.println(TerminalColor.YELLOW + "Escolha uma " + "opção de 1 - 7 \n" + TerminalColor.RESET+ "1 - CADASTRAR PACIENTE \n"  + "2- VISUALIZAR FILAS \n" 
+				 + "3- CHAMAR PRÓXIMO PACIENTE \n"+"4- CONSULTAR PRÓXIMO PACIENTE \n" + 
+				 "5- CONSULTAR IMINÊNCIA PELO CPF\n"+
+				 "6- EXIBIR DADOS ESTATÍSTICOS \n" + "7 - SAIR\n\n"+ "Digite sua opção: ");
 						return sc.nextInt();
 		
 	 }
